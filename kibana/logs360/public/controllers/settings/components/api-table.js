78561730e15c4e5ebc0ca46f6f22d8ecb649d10b
Anderson Goncalves <anderson.coder@gmail.com>
2019-10-20 19:54:04 -0300

    Fix Traducao do Menu API

--- kibana/logs360/public/controllers/settings/components/api-table.js
+++ kibana/logs360/public/controllers/settings/components/api-table.js
@@ -148,16 +148,16 @@ export class ApiTable extends Component {
       },
       {
         field: '_source.api_port',
-        name: 'Port',
+        name: 'Porta',
         align: 'left'
       },
       {
         field: '_source.api_user',
-        name: 'User',
+        name: 'Usuario',
         align: 'left'
       },
       {
-        name: 'Actions',
+        name: 'Ações',
         render: item => (
           <EuiFlexGroup>
             <EuiFlexItem grow={false}>
